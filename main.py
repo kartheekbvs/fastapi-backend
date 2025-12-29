1548c1c6cc8bc0a1deefaf88134398303097f929
Bhamidipati Venkata Sai Kartheek <159104027+kartheekbvs@users.noreply.github.com>
2025-12-29 10:31:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello from Render FastAPI"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (safe for now)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
+def hello(nm: str):
+    return {"message": f"Hello {nm}"}
